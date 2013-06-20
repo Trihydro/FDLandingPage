532fa182d64bb5abd904aa271b42f601594ef37b
Edrec <Nenoedrec@gmail.com>
2013-06-20 15:24:29 -0600

    More code

---
+++ js/app.js
@@ -0,0 +1,57 @@
+App = Ember.Application.create();
+
+App.Store = DS.Store.extend({
+	revision: 12,
+	adapter: 'DS.FixtureAdapter'
+});
+
+App.Router.map(function(){
+	this.resource('tab');
+});
+
+App.TabRoute = Ember.Route.extend({
+	model: function(){
+		return App.Tab.find();
+	}
+});
+
+App.IndexRoute = Ember.Route.extend({
+	redirect: function(){
+		this.transitionTo('tab');
+	}
+});
+
+App.TabController = Ember.ObjectController.extend({
+	1: function(){
+		alert("here");
+	},
+	features: function(){
+		alert("here2");
+	},
+	getting: function(){
+		alert("here3");
+	}
+});
+
+
+App.Tab = DS.Model.extend({
+	header1: DS.attr('string'),
+	header2: DS.attr('string'),
+	description: DS.attr('string')
+	//image: DS.attr('image')
+});
+
+App.Tab.FIXTURES = [{
+	id:1,
+	header1: 'General Information',
+	header2: 'Field Direct Overview'
+},{
+	id:2,
+	header1: 'Features & Benefits',
+	header2: 'Details of Field Direct'
+},{
+	id:3,
+	header1: 'Getting Started',
+	header2: 'Using Field Direct'
+}];
+
